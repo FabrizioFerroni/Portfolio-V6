38bb934c1b1b76a6a1f255b41d506f02c249ceb3
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2022-10-17 23:25:55 -0300

    Se termino parte de inicio y quien soy de estilizar ( desktop )

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+/* cuando hago click .button .nav Toggle 'activo'*/
+// const button = document.querySelector('.button')
+// const nav = document.querySelector('.nav')
+
+// button.addEventListener('click', () => {
+//   nav.classList.toggle('activo')
+// })
