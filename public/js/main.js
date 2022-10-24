a436f8bc0a2f9fcd88a935812628475d284066be
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2022-10-24 13:32:45 -0300

    Se termino de codificar movil y algunos estilos

--- public/js/main.js
+++ public/js/main.js
@@ -287,3 +287,21 @@ cambiar__idioma__movil.addEventListener("click", () => {
         flag__lang__movil.dataset.lang = 'usa';
     }
 });
+
+$(document).ready(function() { irArriba(); });
+
+function irArriba() {
+    $('.ir-arriba').hide();
+    $('.ir-arriba').click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }) /*, 1000 */
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.ir-arriba').fadeIn();
+        } else {
+            $('.ir-arriba').fadeOut();
+        }
+    });
+}
